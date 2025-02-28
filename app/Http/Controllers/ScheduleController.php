dbedb03835f6b2342b9b5573b4b1f3a283a0b67e
Hilmi Al Muhtade <hilmialmuhtadeb@gmail.com>
2025-02-28 14:42:40 +0700

    finishing room schedule information page

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -11,15 +11,28 @@ class ScheduleController extends Controller
 {
     public function index(Request $request)
     {
-        $slug = $request->query('room') ? $request->query('room') : 'inovasi';
-        $room = Room::where('slug', $slug)->first();
-        $events = Book::where('room_id', $room->id)->get();
+        // $slug = $request->query('room') ? $request->query('room') : 'inovasi';
+        // $room = Room::where('slug', $slug)->first();
+        // $events = Book::with('requester.organization')->where('room_id', $room->id)->get();
+        $events = Book::with('requester.organization', 'room')->get();
         $rooms = Room::all();
+
+        if(!empty($request->query('room'))) {
+            $slug = $request->query('room') ? $request->query('room') : 'inovasi';
+            $room = Room::where('slug', $slug)->first();
+            $events = Book::with('requester.organization', 'room')->where('room_id', $room->id)->get();
+
+            return Inertia::render('Schedule', [
+                'events' => $events,
+                'rooms' => $rooms,
+                'selectedRoom' => $slug,
+            ]);
+        }
 
         return Inertia::render('Schedule', [
             'events' => $events,
-            'selectedRoom' => $slug,
-            'rooms' => $rooms
+            'rooms' => $rooms,
+            'selectedRoom' => '',
         ]);
     }
 }
